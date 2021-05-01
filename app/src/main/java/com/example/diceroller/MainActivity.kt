53038235273abaa8f9f56091c962212391b1c1b4
bhadreshpsavani <bhadreshpsavani@gmail.com>
2021-05-01 23:55:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "The Button is Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
